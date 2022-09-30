798e0ebb56538955ec5bb09b9d04b41ddc7f824f
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-09-30 15:47:08 -0700

    saved users

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 
 const SignIn = () => {
@@ -16,14 +17,54 @@ const SignIn = () => {
 
   const navigate = useNavigate()
 
+  const onChange = (e) =>{
+      setFormData((prevState) =>({
+        ...prevState,
+        [e.target.id]: e.target.value
+      }))
+  }
+
   return (
     <>
-      <div className="pageConatiner">
+      <div className="pageContainer">
         <header>
           <p className="pageHeader">
             Welcome Back!  
           </p>
         </header>
+        <main>
+          <form >
+              <input type="email" className="emailInput" 
+                placeholder='Email' id='email' value={email}
+                onChange={onChange}
+              />
+              <div className="passwordInputDiv">
+                <input type={showPassword ? "text" : "password"} className="passwordInput"
+                  placeholder='Password' id='password' value={password} onChange={onChange}
+                />
+                <img src={visibilityIcon}
+                className="showPassword"
+                alt="show password"
+                onClick={() => setShowShowPassword((prevState) => !prevState)}
+                 />
+              </div>
+              <Link to="/forgot-password" className='forgotPasswordLink'> 
+                Forgot Password
+              </Link>
+
+              <div className="signInBar">
+                <p className="signInText">
+                  Sign In
+                </p>
+                <button className="signInButton">
+                  <ArrowRightIcon fill='#fffff' width="34px" height="34px"/>
+                </button>
+              </div>
+          </form>
+          {/* Oauth */}
+
+          <Link to="/sign-up" className='registerLink'>Sign Up Instead</Link>
+        </main>
       </div>
     </>
 
